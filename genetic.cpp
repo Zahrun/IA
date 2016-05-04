63753db1c9bd5290d47c9c2c6f62c85cc515df44
Zahrun <s141842@student.dtu.dk>
2016-05-04 21:56:48 +0200

    ajout threads

---
+++ genetic.cpp
@@ -0,0 +1,13 @@
+/*
+ * genetic.cpp
+ *
+ *  Created on: 4 mai 2016
+ *      Author: aroun
+ */
+
+#include "genetic.h"
+
+void genetic(string msg){
+	cout << "genetic says " << msg << endl;
+}
+
---
+++ genetic.h
@@ -0,0 +1,19 @@
+/*
+ * genetic.h
+ *
+ *  Created on: 4 mai 2016
+ *      Author: aroun
+ */
+
+#ifndef GENETIC_H_
+#define GENETIC_H_
+
+#include <iostream>
+
+using namespace std;
+
+void genetic(string);
+
+
+
+#endif /* GENETIC_H_ */
